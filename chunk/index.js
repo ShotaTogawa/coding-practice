8b10711e17dcf35c61b903c15222385a2fdb85aa
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-11 08:01:22 -0800

    Dec 11

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,48 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const parentArray = [];
+  let childArray = [];
+
+  // loop array of argument
+  array.forEach(arr => {
+    if (childArray.length === size) {
+      parentArray.push(childArray);
+      childArray = [];
+    }
+    childArray.push(arr);
+  });
+  parentArray.push(childArray);
+  return parentArray;
+}
+
+function solution1(array, size) {
+  const chunked = [];
+  for (let element of array) {
+    const last = chunked[chunked.length - 1];
+
+    if (!last || last.length === size) {
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }
+  return chunked;
+}
+
+function solution2(array, size) {
+  const chunked = [];
+  let index = 0;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
+console.log(solution1([1, 2, 3, 4, 5], 2));
+console.log(solution2([1, 2, 3, 4, 5], 2));
 
 module.exports = chunk;
