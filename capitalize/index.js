d71eccc669b9e2d2a58cff58447e26fa15766323
ShotaTogawa <s.togawa0112@gmail.com>
2019-12-13 11:21:37 -0800

    fizzbuzz and capitalized

--- capitalize/index.js
+++ capitalize/index.js
@@ -7,6 +7,34 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let each_words = str.split(" ");
+  let sentence = "";
+  for (word of each_words) {
+    sentence += word.charAt(0).toUpperCase() + word.slice(1) + " ";
+  }
+  return sentence;
+}
 
+const solution1 = str => {
+  const words = [];
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+  return words.join(" ");
+};
+
+const solution2 = str => {
+  let result = str[0].toUpperCase();
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === " ") {
+      result += str[i].toUpperCase();
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
+};
+
+console.log(solution1("apple pen"));
 module.exports = capitalize;
