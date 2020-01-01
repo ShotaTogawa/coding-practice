aa1354eddfdb3ebc29491b9a45dc3f226c0cf59e
ShotaTogawa <s.togawa0112@gmail.com>
2020-01-01 07:33:29 -0800

    finished section 11

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,57 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  let stair = "";
+  for (let i = 1; i <= n; i++) {
+    stair += "#";
+    if (stair.length !== n) {
+      let spaceNum = n - stair.length;
+      let space = "";
+      for (let j = 0; j < spaceNum; j++) {
+        space += " ";
+      }
+      console.log(stair + space);
+    } else {
+      console.log(stair);
+    }
+  }
+}
+
+function solution(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    for (let col = 0; col < n; col++) {
+      if (col <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
+
+function solution2(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+  if (n === stair.length) {
+    console.log(stair);
+    return steps(n, row + 1);
+  }
+
+  //   if (stair.length <= row) {
+  //     stair += "#";
+  //   } else {
+  //     stair += " ";
+  //   }
+  const add = stair.length <= row ? "#" : " ";
+  steps(n, row, stair + add);
+}
+
+steps(5);
+solution(5);
+solution2(5);
 
 module.exports = steps;
